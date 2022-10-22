d2732baa950d60bfee9fbebbe60ce26924052f50
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-10-22 21:41:22 +0200

    initialProject

---
+++ reddit/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<share/FLTSharePlugin.h>)
+#import <share/FLTSharePlugin.h>
+#else
+@import share;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTSharePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharePlugin"]];
+}
+
+@end
--- reddit/lib/main.dart
+++ reddit/lib/main.dart
@@ -1,43 +1,14 @@
-
-/// this is a scrollable screen contains some posts
-/// @author Abdelaziz Salah
-/// @date 14/10/2022
 import 'package:flutter/material.dart';
-import 'Widgets/main_page.dart';
 
-import 'package:reddit/posts_screen.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(const Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      /// these are the routes of my app so that I can navigate through them
-      /// easily using the pushNamed method
-      /// and '/' indicates the home page of the app
-      routes: {'/': (context) => const MainPage()},
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          textTheme: const TextTheme(
-              bodyLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              bodyMedium: TextStyle(
-                fontSize: 18,
-              ),
-              titleLarge: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                  color: Colors.white,
-                  shadows: [
-                    Shadow(
-                        blurRadius: 100,
-                        color: Colors.black,
-                        offset: Offset(10, 10))
-                  ]))),
-
-    );
+    return Container();
   }
 }
