bb91ed7ca6f8a2a7d829552f4fca6f0b3e096939
Sarah Elzayat <sarahelzayat@outlook.com>
2022-10-23 20:44:09 +0200

    web color palette

---
+++ reddit/lib/Components/web_color_manager.dart
@@ -0,0 +1,42 @@
+/// Author @SarahElzayat
+/// Date: 23/10/2022
+/// Color palette constants of Reddit
+
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffcc3700); //primary color of the application
+
+const hoverOrange = Color(0xffbe3502); //orange buttons when hovered
+
+const darkGrey = Color(0xff181a1b); //posts background, colorless buttons
+
+const lightGrey = Color(0xff8d857b); //text in appbar, small headlines and icons
+
+const blueGrey = Color(0xff191f25); //body background, text form fields when hovered
+
+const hoverGrey = Color(0xff171d21); //colorless buttons when hovered
+
+const grey = Color(0xff2f3435); //buttons when hovered
+
+const disabledButtonGrey = Color(0xff5f5f5f); //buttons when hovered
+const disabledHoverButtonGrey = Color(0xff5a5a5a); //buttons when hovered
+
+const deepDarkGrey = Color(0xff1d1f20); //text form field when not focused
+
+const eggshellWhite = Color(0xffd6d3cd); //general text
+
+const blue = Color(0xff47aef8); //blue buttons, some borders, selected filter(hot, trending...)
+
+const downvoteBlue = Color(0xff69adff); //arrow color when downvote is pressed
+const upvoteRed = Color(0xffff581a); //arrow color when upvote is pressed
+
+const hoverBlue = Color(0xff025b9e); //blue buttons when hovered
+
+//red and orange are used together as a gradient background for some buttons
+const gradientRed = Color(0xffbd051c);
+const gradientOrange = Color(0xffbe5c01);
+
+const green = Color(0xff55d56d); //news growing up arrow, online, some buttons
+const red = Color(0xffff284c); //news growing down arrow
+
+const notificationRed = Color(0xffcc3700); //news growing down arrow
