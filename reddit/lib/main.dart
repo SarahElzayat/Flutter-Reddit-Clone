81ef89695b54c462d4deb49b34533f6e8a96b051
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-10-27 01:16:13 +0200

    small changes

--- reddit/lib/main.dart
+++ reddit/lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'Screens/testing_screen.dart';
-
 import 'package:reddit/components/search_field.dart';
 
 import 'consts/colors.dart';
-
 
 void main() {
   runApp(Main());
@@ -19,13 +15,13 @@ class Main extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          scaffoldBackgroundColor: backgroundColor,
-          // inputDecorationTheme: const InputDecorationTheme(
-          //   hintStyle: TextStyle(color: greyColor),
-          //   alignLabelWithHint: true,
-          // ),
+        scaffoldBackgroundColor: backgroundColor,
+        // inputDecorationTheme: const InputDecorationTheme(
+        //   hintStyle: TextStyle(color: greyColor),
+        //   alignLabelWithHint: true,
+        // ),
       ),
-          // iconTheme: IconThemeData(color: ColorManager.lightGrey, size: 20)),
+      // iconTheme: IconThemeData(color: ColorManager.lightGrey, size: 20)),
       home: Scaffold(
         body: SafeArea(
           child: Center(
@@ -37,12 +33,13 @@ class Main extends StatelessWidget {
                   SearchFiled(
                     textEditingController: TextEditingController(),
                   ),
-                  const SizedBox(height: 20,),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   SearchFiled(
                     textEditingController: _controller,
                     isProfile: true,
                     labelText: 'u/sarsora',
-
                   ),
                 ],
               ),
