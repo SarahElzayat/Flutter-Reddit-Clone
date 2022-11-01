3e6748ee4dca398ad0626b5a222b0d7f002e1746
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-11-01 15:11:57 +0200

    adding theme to main and adding white color to Color manager

--- reddit/lib/main.dart
+++ reddit/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:reddit/screens/testing_screen.dart';
 
+import 'components/helpers/color_manager.dart';
+
 void main() {
   runApp(const Main());
 }
@@ -10,6 +12,18 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const TestingScreen();
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: const TextTheme(
+              titleMedium: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: ColorManager.white,
+              ),
+              bodyMedium:
+                  TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+      home: const TestingScreen(),
+    );
+    ;
   }
 }
