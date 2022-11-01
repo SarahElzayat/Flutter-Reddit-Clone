2b9533adea23d9d567c9a146aa3c7c2ec9aea0ba
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-11-01 12:45:50 +0200

    adding some consts to main

--- reddit/lib/main.dart
+++ reddit/lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:reddit/screens/bottom_sheet_test.dart';
 
 void main() {
-  runApp(Main());
+  runApp(const Main());
 }
 
 class Main extends StatelessWidget {
-  Main({super.key});
-  final TextEditingController _controller = TextEditingController();
+  const Main({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return TestScreen();
+    return const TestScreen();
   }
 }
