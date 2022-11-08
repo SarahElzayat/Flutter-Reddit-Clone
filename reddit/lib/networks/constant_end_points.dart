d0c65d5984f8dfc8c7568f94d8137687f563e89d
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-11-08 23:15:00 +0200

    adding new base url and adding sign in for web screem

--- reddit/lib/networks/constant_end_points.dart
+++ reddit/lib/networks/constant_end_points.dart
@@ -2,9 +2,9 @@
 /// @date 3/11/2022
 /// this is a file contaning the constant baseURL and the endpoints of the API
 
-const portNumber = String.fromEnvironment('portNumber', defaultValue: '5000');
+const portNumber = String.fromEnvironment('portNumber', defaultValue: '3000');
 const baseUrl = String.fromEnvironment('baseURL',
-    defaultValue: 'https://dbc799b3-c5d4-4dc1-8421-8e8fd01421ef.mock.pstmn.io');
+    defaultValue: 'http://localhost:3000');
 const signUp = '/signup';
 const login = '/login';
 const loginForgetPassword = '/login/forget-password';
