3e398e28e2c494565b5937643e58457a036059c1
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-11-10 15:49:28 +0200

    I have no Idea what is gonna happens after pushing

---
+++ lib/Screens/sign_in_and_sign_up_screen/sign_up_screen.dart
@@ -0,0 +1,164 @@
+/// @author Abdelaziz Salah
+/// @date 3/11/2022
+/// this is the screen of creating new account for the users.
+
+import 'package:flutter/material.dart';
+import 'package:reddit/data/sign_in_And_sign_up_models/validators.dart';
+import '../../widgets/sign_in_and_sign_up_widgets/continue_button.dart';
+import '../../widgets/sign_in_and_sign_up_widgets/continue_with_facebook_or_google.dart';
+import 'sign_in_screen.dart';
+import '../../data/sign_in_And_sign_up_models/sign_up_model.dart';
+import '../../networks/constant_end_points.dart';
+import '../../networks/dio_helper.dart';
+import '../../components/default_text_field.dart';
+import '../../components/helpers/color_manager.dart';
+import '../../widgets/sign_in_and_sign_up_widgets/app_bar.dart';
+
+// ignore: must_be_immutable
+class SignUpScreen extends StatefulWidget {
+  SignUpScreen({super.key});
+
+  static const routeName = '/sign_up_route';
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController usernameController = TextEditingController();
+
+  TextEditingController passwordController = TextEditingController();
+
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final navigator = Navigator.of(context);
+    final customAppBar = LogInAppBar(
+        sideBarButtonText: 'Log in',
+        sideBarButtonAction: () {
+          navigator.pushReplacementNamed(SignInScreen.routeName);
+        });
+    final textScaleFactor = mediaQuery.textScaleFactor;
+
+    return Scaffold(
+      appBar: customAppBar,
+      backgroundColor: ColorManager.darkGrey,
+      body: SingleChildScrollView(
+        child: Container(
+          /// the height of the screen should be the whole height of the screen
+          /// but without the height of the app bar and without the padding of
+          /// the down drag top of the phone itself
+          height: mediaQuery.size.height -
+              customAppBar.preferredSize.height -
+              mediaQuery.padding.top,
+          width: mediaQuery.size.width,
+          padding: const EdgeInsets.only(top: 30, left: 15, right: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Text(
+                  textAlign: TextAlign.center,
+                  'Hello new friend, welcome to Reddit',
+                  // style: theme.textTheme.titleMedium,
+                  style: TextStyle(
+                    fontSize: textScaleFactor * 24,
+                    fontWeight: FontWeight.bold,
+                    color: ColorManager.white,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: SizedBox(
+                    height: mediaQuery.size.height * 0.18,
+                    child: const ContinueWithGoOrFB()),
+              ),
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    DefaultTextField(
+                      formController: emailController,
+                      labelText: 'Email',
+                      icon: emailController.text.isNotEmpty
+                          ? IconButton(
+                              icon: const Icon(Icons.clear_rounded),
+                              onPressed: (() {
+                                setState(() {
+                                  emailController.text = '';
+                                });
+                              }))
+                          : null,
+                    ),
+                    DefaultTextField(
+                      formController: usernameController,
+                      labelText: 'Username',
+                      icon: usernameController.text.isNotEmpty ||
+                              usernameController.text != ''
+                          ? IconButton(
+                              icon: const Icon(Icons.clear_rounded),
+                              onPressed: (() {
+                                setState(() {
+                                  usernameController.text = '';
+                                });
+                              }))
+                          : null,
+                    ),
+                    DefaultTextField(
+                      formController: passwordController,
+                      labelText: 'Password',
+                      isPassword: true,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text(
+                      textAlign: TextAlign.center,
+                      'By continuing, you agree to our '
+                      'User Agreement and Privace Policy',
+                      style: TextStyle(color: ColorManager.white),
+                    ),
+                    ContinueButton(
+                      isPressable: emailController.text.isNotEmpty &&
+                          usernameController.text.isNotEmpty &&
+                          passwordController.text.isNotEmpty,
+                      appliedFunction: () async {
+                        if (Validator.validEmailValidator(
+                                emailController.text) &&
+                            Validator.validPasswordValidation(
+                                passwordController.text) &&
+                            Validator.validUserName(usernameController.text)) {
+                          /// TODO: remove this log out from here it is not its place
+                          // await FacebookLoginAPI.logOut();
+                          final user = SignUpModel(
+                              email: emailController.text,
+                              password: passwordController.text,
+                              username: usernameController.text);
+
+                          DioHelper.postData(path: login, data: user.toJson())
+                              .then((value) {
+                            /// here we want to make sure that we got the correct response
+                          });
+                        } else {
+                          print('SomeThing went wrong');
+                        }
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/sign_in_and_sign_up_widgets/continue_with_facebook_or_google.dart
@@ -0,0 +1,100 @@
+/// @author Abdelaziz Salah
+/// @date 6/11/2022
+/// these are the two buttons of continue with
+///  facebook and containue with google.
+
+import 'package:flutter/material.dart';
+import '../../components/Helpers/color_manager.dart';
+import '../../data/facebook_api/facebook_api.dart';
+import '../../data/google_api/google_sign_in_api.dart';
+
+class ContinueWithGoOrFB extends StatelessWidget {
+  const ContinueWithGoOrFB({super.key});
+
+  Future signInWithGoogle() async {
+    await GoogleSignInApi.login();
+  }
+
+  Future signInWithFacebook() async {
+    await FacebookLoginAPI.login();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        ElevatedButton(
+            style: const ButtonStyle(
+                shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    side: BorderSide(width: 2, color: ColorManager.white))),
+                backgroundColor:
+                    MaterialStatePropertyAll(ColorManager.darkGrey)),
+            onPressed: signInWithGoogle,
+            child: Row(
+              children: [
+                SizedBox(
+                  height: mediaQuery.size.height * 0.04,
+                  child: Image.asset(
+                    'assets/icons/googleIcon.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(
+                  width: mediaQuery.size.width * 0.15,
+                ),
+                Text(
+                  'Continue with google',
+                  style: TextStyle(
+                    color: ColorManager.white,
+                    fontSize: (18 * mediaQuery.textScaleFactor),
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            )),
+        ElevatedButton(
+            style: const ButtonStyle(
+                shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    side: BorderSide(width: 2, color: ColorManager.white))),
+                backgroundColor:
+                    MaterialStatePropertyAll(ColorManager.darkGrey)),
+            onPressed: signInWithFacebook,
+            child: Row(
+              children: [
+                SizedBox(
+                  height: mediaQuery.size.height * 0.035,
+                  child: Image.asset(
+                    'assets/icons/facebookIcon.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(
+                  width: mediaQuery.size.width * 0.15,
+                ),
+                Text(
+                  'Continue with facebook',
+                  style: TextStyle(
+                    color: ColorManager.white,
+                    fontSize: (18 * mediaQuery.textScaleFactor),
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            )),
+        Text(
+          'OR',
+          style: TextStyle(
+            color: ColorManager.greyColor,
+            fontWeight: FontWeight.bold,
+            fontSize: 14 * mediaQuery.textScaleFactor,
+          ),
+        ),
+      ],
+    );
+  }
+}
