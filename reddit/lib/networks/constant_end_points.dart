4197e0f2956de6324bfc56ecfa70b9b20e3a4628
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-12-02 16:49:27 +0200

    modifying the base URL

--- reddit/lib/networks/constant_end_points.dart
+++ reddit/lib/networks/constant_end_points.dart
@@ -4,7 +4,7 @@
 
 const portNumber = String.fromEnvironment('portNumber', defaultValue: '5000');
 const baseUrl =
-    String.fromEnvironment('baseURL', defaultValue: 'http://10.0.2.2:3000');
+    String.fromEnvironment('BASE_URL', defaultValue: 'http://10.0.2.2:3000');
 
 const signUp = '/signup';
 const login = '/login';
