da633d76d56e38c97ee796feb8d437d2ce8fda09
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-12-02 18:51:10 +0200

    env variables replaced

--- reddit/lib/networks/constant_end_points.dart
+++ reddit/lib/networks/constant_end_points.dart
@@ -1,10 +1,14 @@
 /// @author Abdelaziz Salah
 /// @date 3/11/2022
 /// this is a file contaning the constant baseURL and the endpoints of the API
+import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode;
 
 const portNumber = String.fromEnvironment('portNumber', defaultValue: '5000');
-const baseUrl =
-    String.fromEnvironment('BASE_URL', defaultValue: 'http://10.0.2.2:3000');
+const baseUrl = kReleaseMode
+    ? 'http://read-it.francecentral.cloudapp.azure.com/api/'
+    : kIsWeb
+        ? 'http://localhost:3000'
+        : 'http://10.0.2.2:3000';
 
 const signUp = '/signup';
 const login = '/login';
