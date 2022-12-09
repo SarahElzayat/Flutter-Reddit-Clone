34c5e34149e2d96fcdbda7ebfe10e23ddc455060
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-12-10 00:51:28 +0200

    Add comment

--- reddit/lib/screens/comments/add_comment_screen.dart
+++ reddit/lib/screens/comments/add_comment_screen.dart
@@ -1,12 +1,19 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' hide Text;
 import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:reddit/components/helpers/color_manager.dart';
-import 'package:flutter_quill/flutter_quill.dart' as quill;
+import 'package:path/path.dart';
 
 import '../../data/comment/comment_model.dart';
 import '../../data/post_model/post_model.dart';
+
+import 'dart:async';
 
 class AddCommentScreen extends StatefulWidget {
   static const routeName = 'add-comment';
@@ -24,16 +31,58 @@ class AddCommentScreen extends StatefulWidget {
 }
 
 class _AddCommentScreenState extends State<AddCommentScreen> {
-  quill.QuillController? _controller;
-
+  QuillController? _controller;
+  final FocusNode _focusNode = FocusNode();
   @override
   void initState() {
-    _controller = quill.QuillController.basic();
+    _controller = QuillController.basic();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    var toolbar = QuillToolbar.basic(
+      showUndo: false,
+      showRedo: false,
+      showBoldButton: false,
+      showItalicButton: false,
+      showBackgroundColorButton: false,
+      showCenterAlignment: false,
+      showLeftAlignment: false,
+      showRightAlignment: false,
+      showJustifyAlignment: false,
+      showHeaderStyle: false,
+      showListNumbers: false,
+      showListBullets: false,
+      showCodeBlock: false,
+      showStrikeThrough: false,
+      showFontSize: false,
+      controller: _controller!,
+      embedButtons: FlutterQuillEmbeds.buttons(
+        // provide a callback to enable picking images from device.
+        // if omit, "image" button only allows adding images from url.
+        // same goes for videos.
+        onImagePickCallback: _onImagePickCallback,
+        onVideoPickCallback: _onVideoPickCallback,
+        // uncomment to provide a custom "pick from" dialog.
+        // mediaPickSettingSelector: _selectMediaPickSetting,
+        // uncomment to provide a custom "pick from" dialog.
+        // cameraPickSettingSelector: _selectCameraPickSetting,
+      ),
+      showAlignmentButtons: true,
+      afterButtonPressed: _focusNode.requestFocus,
+    );
+    if (kIsWeb) {
+      toolbar = QuillToolbar.basic(
+        controller: _controller!,
+        embedButtons: FlutterQuillEmbeds.buttons(
+          onImagePickCallback: _onImagePickCallback,
+          webImagePickImpl: _webImagePickImpl,
+        ),
+        showAlignmentButtons: true,
+        afterButtonPressed: _focusNode.requestFocus,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add comment'),
@@ -49,10 +98,10 @@ class _AddCommentScreenState extends State<AddCommentScreen> {
       ),
       body: Column(
         children: [
-          quill.QuillEditor(
-            controller: quill.QuillController(
-              document: quill.Document.fromJson(
-                  jsonDecode(widget.post.content ?? '[]')),
+          QuillEditor(
+            controller: QuillController(
+              document:
+                  Document.fromJson(jsonDecode(widget.post.content ?? '[]')),
               selection: const TextSelection(baseOffset: 0, extentOffset: 0),
             ),
             readOnly: true,
@@ -72,7 +121,7 @@ class _AddCommentScreenState extends State<AddCommentScreen> {
             thickness: 1,
           ),
           Expanded(
-            child: quill.QuillEditor(
+            child: QuillEditor(
               controller: _controller!,
               readOnly: false,
               autoFocus: true,
@@ -86,8 +135,45 @@ class _AddCommentScreenState extends State<AddCommentScreen> {
               ],
             ),
           ),
+          toolbar
         ],
       ),
     );
+  }
+
+  // Renders the image picked by imagePicker from local file storage
+  // You can also upload the picked image to any server (eg : AWS s3
+  // or Firebase) and then return the uploaded image URL.
+  Future<String> _onImagePickCallback(File file) async {
+    // Copies the picked file from temporary cache to applications directory
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final copiedFile =
+        await file.copy('${appDocDir.path}/${basename(file.path)}');
+    return copiedFile.path.toString();
+  }
+
+  Future<String?> _webImagePickImpl(
+      OnImagePickCallback onImagePickCallback) async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) {
+      return null;
+    }
+
+    // Take first, because we don't allow picking multiple files.
+    final fileName = result.files.first.name;
+    final file = File(fileName);
+
+    return onImagePickCallback(file);
+  }
+
+  // Renders the video picked by imagePicker from local file storage
+  // You can also upload the picked video to any server (eg : AWS s3
+  // or Firebase) and then return the uploaded video URL.
+  Future<String> _onVideoPickCallback(File file) async {
+    // Copies the picked file from temporary cache to applications directory
+    final appDocDir = await getApplicationDocumentsDirectory();
+    final copiedFile =
+        await file.copy('${appDocDir.path}/${basename(file.path)}');
+    return copiedFile.path.toString();
   }
 }
