65eea6935ce36777a9169ba0a96e9a4f210cb86b
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-12-12 11:36:34 +0200

    fixed a bug

--- reddit/lib/widgets/comments/comment.dart
+++ reddit/lib/widgets/comments/comment.dart
@@ -6,12 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
-import 'package:logger/logger.dart';
 import 'package:reddit/components/helpers/color_manager.dart';
 import 'package:reddit/cubit/post_notifier/post_notifier_cubit.dart';
 import 'package:reddit/cubit/post_notifier/post_notifier_state.dart';
 import 'package:reddit/data/comment/comment_model.dart';
 import 'package:reddit/functions/post_functions.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
 import 'package:reddit/screens/comments/add_comment_screen.dart';
 import 'package:reddit/screens/posts/post_screen_cubit/post_screen_cubit.dart';
 import 'package:reddit/widgets/posts/actions_cubit/post_comment_actions_cubit.dart';
@@ -107,101 +108,117 @@ class _CommentState extends State<Comment> {
           ),
         ),
       ],
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border(
-            left: widget.level > 1
-                ? BorderSide(
-                    color: ColorManager.lightGrey,
-                    width: 0.5.w,
-                  )
-                : BorderSide.none,
+      child: InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onLongPress: () {
+          setState(() {
+            isCompressed = !isCompressed;
+          });
+        },
+        onTap: () {
+          if (isCompressed) {
+            setState(() {
+              isCompressed = !isCompressed;
+            });
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              left: widget.level > 1
+                  ? BorderSide(
+                      color: ColorManager.lightGrey,
+                      width: 0.5.w,
+                    )
+                  : BorderSide.none,
+            ),
+            color: ColorManager.darkGrey,
           ),
-          color: ColorManager.darkGrey,
-        ),
-        padding: EdgeInsets.only(
-          left: 10,
-          right: widget.level == 1 ? 10 : 0,
-          top: 5,
-          bottom: 5,
-        ),
+          padding: EdgeInsets.only(
+            left: 10,
+            right: widget.level == 1 ? 10 : 0,
+            top: 5,
+            bottom: 5,
+          ),
 
-        // margin: EdgeInsets.only(left: widget.level * 10.0),
-        child: ConditionalBuilder(
-          condition: isCompressed,
-          builder: (context) {
-            return Row(
-              children: [
-                singleRow(post: widget.post, showIcon: true),
-              ],
-            );
-          },
-          fallback: (context) {
-            return Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: singleRow(
-                        post: widget.post,
-                        showDots: false,
-                        showIcon: true,
+          // margin: EdgeInsets.only(left: widget.level * 10.0),
+          child: ConditionalBuilder(
+            condition: isCompressed,
+            builder: (context) {
+              return commentAsRow(
+                  post: widget.comment,
+                  showContent: true,
+                  content: _controller!.document
+                      .toPlainText()
+                      .replaceAll('\\n', ''));
+            },
+            fallback: (context) {
+              return Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: commentAsRow(
+                          post: widget.comment,
+                          showDots: false,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                quill.QuillEditor(
-                  controller: _controller!,
-                  readOnly: true,
-                  autoFocus: false,
-                  expands: false,
-                  scrollable: false,
-                  scrollController: ScrollController(),
-                  focusNode: FocusNode(),
-                  padding: EdgeInsets.zero,
-                  embedBuilders: [
-                    ...FlutterQuillEmbeds.builders(),
-                  ],
-                ),
-                // _commentsRow(),
+                    ],
+                  ),
+                  quill.QuillEditor(
+                    controller: _controller!,
+                    readOnly: true,
+                    autoFocus: false,
+                    expands: false,
+                    scrollable: false,
+                    scrollController: ScrollController(),
+                    focusNode: FocusNode(),
+                    padding: EdgeInsets.zero,
+                    embedBuilders: [
+                      ...FlutterQuillEmbeds.builders(),
+                    ],
+                  ),
+                  // _commentsRow(),
 
-                _commentsControlRow(),
-                widget.comment.children != null
-                    ? Column(
-                        children: widget.comment.children!
-                            .map((e) => Comment(
-                                  post: widget.post,
-                                  comment: e,
-                                  level: widget.level + 1,
-                                ))
-                            .toList(),
-                      )
-                    : Container(),
+                  _commentsControlRow(),
+                  widget.comment.children != null
+                      ? Column(
+                          children: widget.comment.children!
+                              .map((e) => Comment(
+                                    post: widget.post,
+                                    comment: e,
+                                    level: widget.level + 1,
+                                  ))
+                              .toList(),
+                        )
+                      : Container(),
 
-                // if there is more children add a button to show them
-                if (widget.comment.children != null &&
-                    widget.comment.children!.length <
-                        (widget.comment.numberofChildren!))
-                  InkWell(
-                    onTap: () {
-                      PostScreenCubit.get(context).showMoreComments(
-                        commentId: widget.comment.id!,
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.only(top: 5),
-                      child: const Text(
-                        'Show more comments',
-                        style: TextStyle(
-                          color: ColorManager.primaryColor,
-                          fontSize: 12,
+                  // if there is more children add a button to show them
+                  if (widget.comment.children != null &&
+                      widget.comment.children!.length <
+                          (widget.comment.numberofChildren!))
+                    InkWell(
+                      onTap: () {
+                        PostScreenCubit.get(context).showMoreComments(
+                          commentId: widget.comment.id!,
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 5),
+                        child: const Text(
+                          'Show more comments',
+                          style: TextStyle(
+                            color: ColorManager.primaryColor,
+                            fontSize: 12,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-              ],
-            );
-          },
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
@@ -284,6 +301,49 @@ class _CommentState extends State<Comment> {
           },
           icon: const Icon(Icons.arrow_drop_up),
         ),
+      ],
+    );
+  }
+
+  Widget commentAsRow({
+    bool showDots = true,
+    bool showContent = false,
+    String content = '',
+    required CommentModel post,
+  }) {
+    return Row(
+      children: [
+        subredditAvatar(small: true),
+        SizedBox(
+          width: min(5.w, 0.2.dp),
+        ),
+        Text(
+          '${'u'}/${post.commentedBy} ',
+          style: const TextStyle(
+            color: ColorManager.greyColor,
+            fontSize: 15,
+          ),
+        ),
+        Text(
+          '• ${timeago.format(DateTime.tryParse(post.editTime ?? '') ?? DateTime.now(), locale: 'en_short')}',
+          style: const TextStyle(
+            color: ColorManager.greyColor,
+            fontSize: 15,
+          ),
+        ),
+        if (showContent)
+          Expanded(
+            child: Text(
+              ' • $content',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                color: ColorManager.greyColor,
+                fontSize: 15,
+              ),
+            ),
+          ),
+        if (showDots) dropDownDots(post)
       ],
     );
   }
