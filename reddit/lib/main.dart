bc3abe0da84ff1bb2a3426661f27d9a38de2edd9
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-12-19 21:58:39 +0200

    finshing sign up module for mobile

--- reddit/lib/main.dart
+++ reddit/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reddit/components/helpers/enums.dart';
 import 'package:reddit/cubit/user_profile/cubit/user_profile_cubit.dart';
 import 'package:reddit/screens/comments/add_comment_screen.dart';
+import 'package:reddit/screens/sign_in_and_sign_up_screen/mobile/continue_sign_up_for_mobile.dart';
 import 'package:reddit/screens/sign_in_and_sign_up_screen/web/continue_sign_up_screen.dart';
 import 'package:reddit/screens/sign_in_and_sign_up_screen/web/sign_up_for_web_screen.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
