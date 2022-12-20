d20837b25a6715381241653b3e91fb425a9451d3
Abdelaziz Salah <abdelaziz132001@gmail.com>
2022-12-20 22:18:18 +0200

    correct content

--- reddit/lib/screens/inbox/single_message_screen.dart
+++ reddit/lib/screens/inbox/single_message_screen.dart
@@ -4,6 +4,7 @@
 
 import 'dart:async';
 import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart' hide Text;
@@ -11,6 +12,11 @@ import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
 import 'package:logger/logger.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
+import 'package:reddit/data/messages/messages_model.dart';
+import 'package:reddit/data/messages/reply_to_message_model.dart';
+import 'package:reddit/networks/constant_end_points.dart';
+import 'package:reddit/networks/dio_helper.dart';
+import 'package:reddit/shared/local/shared_preferences.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import '../../components/helpers/color_manager.dart';
 
@@ -32,9 +38,63 @@ class _SingleMessageScreenState extends State<SingleMessageScreen> {
     super.initState();
   }
 
+  void sendTheReply(content, MessageChildren msg) async {
+    // build the model of reply
+    ReplyToMessageModel reply = ReplyToMessageModel(
+      text: 'content', // the content of the reply either string or json.
+      subject: msg.data?.subject,
+      receiverUsername: msg.data?.senderUsername,
+      repliedMsgId: msg.id,
+      subredditName: msg.data?.subredditName,
+    );
+
+    // send the reply to the backend
+    await DioHelper.postData(path: replyToMessage, data: reply.toJson())
+        .then((response) {
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: ColorManager.green,
+            content: Text('Reply is sent successfully 😊'),
+          ),
+        );
+
+        // after sending the reply close the message.
+        Navigator.of(context).pop();
+      }
+    }).onError((error, trace) {
+      error = error as DioError;
+      if (!error.message.contains('500')) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: ColorManager.red,
+            content: Text(
+              '${error.response?.data['error']} 😔',
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: ColorManager.red,
+            content: Text(
+              'SomeThing went wrong!😔',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final messageBody = ModalRoute.of(context)!.settings.arguments as String;
+    final myMessage =
+        ModalRoute.of(context)!.settings.arguments as MessageChildren;
+    String? myMessageText = myMessage.data?.text;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Reply to Messages'),
@@ -46,14 +106,18 @@ class _SingleMessageScreenState extends State<SingleMessageScreen> {
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(
                     backgroundColor: ColorManager.yellow,
-                    content: Text('Comment cannot be empty'),
+                    content: Text('Reply cannot be empty 😅',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: ColorManager.darkBlack,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold)),
                   ),
                 );
                 return;
               }
-              final content = _controller!.document.toDelta().toJson();
-              // TODO: SEND THIS TO THE BACKEND
-              Map sentContent = {'ops': content};
+
+              sendTheReply(myMessageText!, myMessage);
             },
             child: const Text(
               'Post',
@@ -65,20 +129,29 @@ class _SingleMessageScreenState extends State<SingleMessageScreen> {
       body: Column(
         children: [
           Expanded(
-            child: QuillEditor(
-              controller: getController(messageBody),
-              readOnly: true,
-              enableInteractiveSelection: true,
-              autoFocus: false,
-              expands: false,
-              scrollable: true,
-              scrollController: ScrollController(),
-              focusNode: FocusNode(),
-              padding: EdgeInsets.zero,
-              embedBuilders: [
-                ...FlutterQuillEmbeds.builders(),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(myMessageText ?? 'empty',
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontSize: 21.sp,
+                    color: ColorManager.eggshellWhite,
+                  )),
             ),
+            // child: QuillEditor(
+            //   controller: getController(myMessage),
+            //   readOnly: true,
+            //   enableInteractiveSelection: true,
+            //   autoFocus: false,
+            //   expands: false,
+            //   scrollable: true,
+            //   scrollController: ScrollController(),
+            //   focusNode: FocusNode(),
+            //   padding: EdgeInsets.zero,
+            //   embedBuilders: [
+            //     ...FlutterQuillEmbeds.builders(),
+            //   ],
+            // ),
           ),
           const Divider(
             height: 10,
@@ -121,12 +194,12 @@ class _SingleMessageScreenState extends State<SingleMessageScreen> {
     );
   }
 
-  QuillController getController(messageBody) {
+  QuillController getController(myMessage) {
     Document doc;
     try {
-      logger.wtf(messageBody.content ?? {'ops': []});
+      logger.wtf(myMessage.content ?? {'ops': []});
       //// this if the message is in json formate
-      // doc = Document.fromJson((messageBody.content ?? {'ops': []})['ops']);
+      // doc = Document.fromJson((myMessage.content ?? {'ops': []})['ops']);
 
       /// this is the message is in string formate
       // doc = Doc;
